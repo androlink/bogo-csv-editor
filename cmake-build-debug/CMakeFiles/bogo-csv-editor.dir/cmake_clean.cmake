3bdf600ade646c553354f83125a0a170c54e6b61
Sigma <59624364+androlink@users.noreply.github.com>
2021-11-06 18:21:31 +0100

    init

---
+++ cmake-build-debug/CMakeFiles/bogo-csv-editor.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/bogo-csv-editor.dir/main.c.obj"
+  "bogo-csv-editor.exe"
+  "bogo-csv-editor.exe.manifest"
+  "bogo-csv-editor.pdb"
+  "libbogo-csv-editor.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/bogo-csv-editor.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.c
@@ -0,0 +1,13 @@
+//
+// Created by Androlink on 06/11/2021.
+//
+
+#include <stdio.h>
+
+int main(){
+
+
+
+
+    return 0;
+}
